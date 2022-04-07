035f6d60e4dfb8ad49adfa36fec523ef9eecd76e
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-06 21:55:13 -0300

    build(app.js): :heavy_plus_sign:Add react-router-dom dependency
    n

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Container from "./assets/style/StyledApp";
+import Drinks from "./pages/drinks/Drinks";
+import Sobre from "./pages/sobre/Sobre";
+import NossoTime from "./pages/nosso-time/NossoTime";
+import Contato from "./pages/contatos/Contatos";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+   return (
+      <Container>
+         <BrowserRouter>
+            <header>
+               <Navbar
+                  item1={"Drinks"}
+                  item2={"Sobre Nós"}
+                  item3={"Nosso Time"}
+                  item4={"Contato"}
+               />
+            </header>
+            <main>
+               <Routes>
+                  <Route path="/" element={<Home />}></Route>
+                  <Route path="/drinks" element={<Drinks />}></Route>
+                  <Route path="/sobre" element={<Sobre />}></Route>
+                  <Route path="/time" element={<NossoTime />}></Route>
+                  <Route path="/contato" element={<Contato />}></Route>
+               </Routes>
+            </main>
+            <footer>
+               <Footer text={"Resilia 2022 - Alguns direitos reservados"} />
+            </footer>
+         </BrowserRouter>
+      </Container>
+   );
 }
 
 export default App;
