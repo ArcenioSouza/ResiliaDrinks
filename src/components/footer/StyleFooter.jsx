8c122b06019c0b9e320295440b5de382b4d35913
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-06 22:04:09 -0300

    style(stylefooter.jsx): :lipstick:Add style to footer

---
+++ src/components/footer/StyleFooter.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyleFooter = styled.div`
+   height: 100%;
+   background-color: var(--yellow);
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   box-shadow: -5px -5px 10px #000;
+
+   p{
+      font-weight: bold;
+      font-size: 1.2rem;
+   }
+`
+
+export default StyleFooter;
