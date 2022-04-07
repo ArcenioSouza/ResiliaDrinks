1ff979c9db99c5291dc6821eb6511f0c0ab01c85
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-06 22:05:24 -0300

    feat(home.jsx): :sparkles:Add page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from './StyledHome'
+import image from '../../assets/img/bg-bons-drinks.jpg'
+
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>Resilia Drinks</h1>
+      <img src={image} alt="" />
+    </Container>
+  )
+}
+
+export default Home
